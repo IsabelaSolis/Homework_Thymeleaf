64e4812724b9abcc26db44a27c72c12055cbc5a2
Isabela Solis <a376827@uach.mx>
2026-02-11 15:24:33 -0600

    Second commit:agregue un booleano para mostrar tablas y cambie la infromacion

--- src/main/java/com/example/demo/controller/Cont.java
+++ src/main/java/com/example/demo/controller/Cont.java
@@ -1,8 +1,15 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+//Aqui lo unico que agregue es el booleano, si es verdadero me va a mostrar la tabla 1 y si es falso la tabla 2
 @Controller
 public class Cont {
-
+	 @GetMapping("/")
+	    public String index(Model model) {
+	        model.addAttribute("Tabla1", false);
+	        // Devuelve el nombre de la vista (index.html)
+	        return "index";
+	    }
 }
